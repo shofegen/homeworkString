cfeb54e35b6792754c326540b6501c235df47ff1
shofegen <danik_lik1@mail.ru>
2023-10-11 20:07:37 +0500

    My homework is done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+// Задача 1
+        String firstName = "Семён ";
+        String middleName = "Семёнович ";
+        String lastName = "Иванов ";
+        String fullName = lastName + firstName + middleName;
+        System.out.println(fullName);
+// Задача 2
+        String small = fullName.toUpperCase();
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + small);
+// Задача 3
+        String s1 = fullName.replace('ё', 'е');
+        System.out.println("Данные Ф. И. О. сотрудника — " + s1);
     }
 }
